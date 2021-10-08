a90bb6aaf02bd470d37226fb5821b8afde1263ed
elidelmar <88523575+elidelmar@users.noreply.github.com>
2021-10-08 14:17:26 -0400

    updates css

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ function displayForecast(response) {
             forecastDay.weather[0].icon
           }@2x.png"
           alt=""
-          width="42"
+          width="45"
         />
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
